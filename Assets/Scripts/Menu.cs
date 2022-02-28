dce19cf4d3cd3a7046016f8810e534f0c49f7508
Julien Brossard <j.brossard@rubika-edu.com>
2022-02-28 16:22:20 +0100

    Add SceneName to Script Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,9 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    [SerializeField] private string resetSceneName;
+    
     public void Reset()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Prototype");
+        SceneManager.LoadScene(resetSceneName);
     }
 }
