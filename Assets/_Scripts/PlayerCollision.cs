abd0b248fd4060a1b3caa838523e801cb43dfcbb
Julien Brossard <j.brossard@rubika-edu.com>
2022-05-25 09:29:57 +0200

    Changement Du Retry Tutorial

--- Assets/_Scripts/PlayerCollision.cs
+++ Assets/_Scripts/PlayerCollision.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private Scene scene;
+    
     private void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Cloud") && !other.gameObject.CompareTag("Paper Piece"))
         {
-            GameManager.instance.Failed();
+            scene = SceneManager.GetActiveScene();
+            if (scene.name == "Tutorial")
+            {
+                TutorialManager.instance.Retry();
+            }
+            else
+            {
+                GameManager.instance.Failed();
+            }
         }
     }
 }
