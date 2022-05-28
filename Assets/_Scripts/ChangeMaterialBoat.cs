f56f58c4766fa4aa7d3b8df6fb252ddbdc163e85
Julien Brossard <j.brossard@rubika-edu.com>
2022-05-28 12:04:00 +0200

    Réglage du bug des skins

---
+++ Assets/_Scripts/ChangeMaterialBoat.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class ChangeMaterialBoat : MonoBehaviour
+{
+    public static GameObject go;
+
+    private void Awake()
+    {
+        go = gameObject;
+    }
+}
